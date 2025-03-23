8074b42b18ae79ca96f54cd27b9f20993b818f9e
orgutier <osvaldoramon14@gmail.com>
2025-03-22 22:24:47 -0600

    Camera code

--- HomeSecCamera/WebCAM_rpz2/main.py
+++ HomeSecCamera/WebCAM_rpz2/main.py
@@ -1,48 +1,18 @@
-## sudo raspi-config
-## Interface -> Camera -> Enable
+from flask import Flask, send_from_directory
+import os
 
-## sudo apt update
-## sudo apt install python3-picamera2 python3-opencv
+app = Flask(__name__)
 
-import http.server
-import socketserver
+IMAGE_DIR = "static"
+IMAGE_NAME = "photo.jpg"  # Change this to your image file name
 
-# Set the port (default is 8000; change if needed)
-PORT = 8000
+@app.route("/")
+def index():
+    return f'<img src="/image" style="max-width:100%; height:auto;">'
 
-# Define a simple HTML page
-html_content = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Raspberry Pi LAN Website</title>
-</head>
-<body>
-    <h1>Hello from Raspberry Pi!</h1>
-    <p>This is a simple website hosted on your Pi.</p>
-</body>
-</html>
-"""
+@app.route("/image")
+def serve_image():
+    return send_from_directory(IMAGE_DIR, IMAGE_NAME)
 
-# Create a custom handler to serve the HTML
-class CustomHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(html_content.encode("utf-8"))
-
-# Function to get the Pi's IP address
-def get_ip():
-    import socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    ip = s.getsockname()[0]
-    s.close()
-    return ip
-
-# Set up the server
-Handler = CustomHandler
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving website at http://{get_ip()}:8000")
-    httpd.serve_forever()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
